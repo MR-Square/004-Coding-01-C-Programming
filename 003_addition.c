c24c8a034db65d8049246c1cdd44152f7874327d
MR-Sauqre <20co53@aiktc.ac.in>
2022-10-27 22:43:56 +0530

    3rd program

---
+++ 003_addition.c
@@ -0,0 +1,54 @@
+//		Addition of two numbers.
+/*
+TITLE : ADDITION OF TWO NUMBERS
+OWNER : MOHD RAZA MOHD RAFIQUE
+DATE  : 27/10/2022
+*/
+#include<stdio.h>
+void main()
+{
+	// First we will take two vairables.
+	int no1,no2;	// This step is called declaration.
+	
+	no1=45;
+	no2 = 13;
+	// Both step is called initialization.
+	/*
+	There are two ways to print addition of two numbers.
+	1. Print directly addition of two numbers.
+	2. Take a new variable and store addition of two numbers in it.
+	*/
+	// Let's see first way
+	
+	printf("Sum = %d\n",no1+no2);
+	
+	// Now let's see second way
+	
+	// In this way we will declare a variable.
+	
+	int sum=no1+no2; 	// This step is called declaration and initilization.
+	printf("Sum = %d",sum);
+	
+	/*
+	Declaration:
+	
+	Step to declare a variable is called Declaration.
+	
+	Initialization:
+	
+	Step to give the value to a variable is called Initialization.
+	
+	\n:	It is use to take new line in output.
+	\t: It is use to take tab(some space) in out put.
+	*/
+	
+	
+}
+
+/*
+ABOUT MY YOUTUBE CHANNEL
+NAME :     MR SQUARE
+LINK   :     https://www.youtube.com/channel/UCFQ-C2iL9J9cbE0X1fawH_w
+*/
+
+
